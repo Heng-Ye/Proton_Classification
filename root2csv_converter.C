1c5b7ce88e40321da63548010b101b8a4fb1ef52
Heng-Ye Liao <liao@phys.ksu.edu>
2022-08-18 22:54:02 -0500

    Add additional parameters

--- root2csv_converter.C
+++ root2csv_converter.C
@@ -11,12 +11,18 @@ using namespace ROOT::Math;
 
 void root2csv_converter() {
 	//setup input file name ---------------------//
-	TString str_file=Form("protons_mva2.root");
+	TString str_filename="protons_with_additionalinfo_mva2";
+	//TString str_file=Form("protons_mva2.root");
+	TString str_file=Form("%s.root",str_filename.Data());
 
 	//setup output file name ---------------------------------//
-	TString str_out_file=Form("protons_mva2.csv");
-	TString str_out_file_train=Form("protons_mva2_train.csv");
-	TString str_out_file_test=Form("protons_mva2_test.csv");
+	//TString str_out_file=Form("protons_mva2.csv");
+	//TString str_out_file_train=Form("protons_mva2_train.csv");
+	//TString str_out_file_test=Form("protons_mva2_test.csv");
+	//
+	TString str_out_file=Form("%s.csv",str_filename.Data());
+	TString str_out_file_train=Form("%s_train.csv",str_filename.Data());
+	TString str_out_file_test=Form("%s_test.csv",str_filename.Data());
 
 	ofstream fout;
 	ofstream fout_train;
@@ -42,6 +48,18 @@ void root2csv_converter() {
 	Float_t calo;
 	Float_t avcalo;
 
+	Float_t st_x;
+	Float_t st_y;
+	Float_t st_z;
+	Float_t end_x;
+	Float_t end_y;
+	Float_t end_z;
+
+	Float_t pbdt;
+	Int_t nd;
+
+
+
  	//set branch address ---------------------------------//
 	tr->SetBranchAddress("train",&train);
 	tr->SetBranchAddress("tag",&tag);
@@ -55,10 +73,28 @@ void root2csv_converter() {
 	tr->SetBranchAddress("calo",&calo);
 	tr->SetBranchAddress("avcalo",&avcalo);
 
+	tr->SetBranchAddress("st_x",&st_x);
+	tr->SetBranchAddress("st_y",&st_y);
+	tr->SetBranchAddress("st_z",&st_z);
+	tr->SetBranchAddress("end_x",&end_x);
+	tr->SetBranchAddress("end_y",&end_y);
+	tr->SetBranchAddress("end_z",&end_z);
+	tr->SetBranchAddress("pbdt",&pbdt);
+	tr->SetBranchAddress("nd",&nd);
+
+
 	//save header to the csv file --------------------------------------------------------//
-  	fout<<"train,tag,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
-  	fout_train<<"tag,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
-  	fout_test<<"tag,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
+	//TString txt_out="train,tag,target,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo";
+	TString txt_out="train,tag,target,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo,st_x,st_y,st_z,end_x,end_y,end_z,pbdt,nd";
+  	//fout<<"train,tag,target,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
+  	//fout_train<<"tag,target,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
+  	//fout_test<<"tag,target,ntrklen,trklen,PID,B,costheta,mediandedx,endpointdedx,calo,avcalo\n";
+
+	fout<<txt_out.Data()<<"\n";
+	fout_train<<txt_out.Data()<<"\n";
+	fout_test<<txt_out.Data()<<"\n";
+
+
 
 	//loop over all events ------------------------------------//
 	Long64_t nentries = tr->GetEntries();
@@ -75,11 +111,17 @@ void root2csv_converter() {
            	isTestSample = false; //Divide MC sample by 2 parts: test+ufold
                         train=0;
            }
+	   int TARGET=0;
+	   if (tag==1) TARGET=1;
+	   		
 
-	   fout<<train<<","<<tag<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
-	   if (!isTestSample) fout_train<<tag<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
-	   if (isTestSample) fout_test<<tag<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
+	   //fout<<train<<","<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
+	   //if (!isTestSample) fout_train<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
+	   //if (isTestSample) fout_test<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<"\n";	
 
+	   fout<<train<<","<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<","<<st_x<<","<<st_y<<","<<st_z<<","<<end_x<<","<<end_y<<","<<end_z<<","<<pbdt<<","<<nd<<"\n";	
+	   if (!isTestSample) fout_train<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<","<<st_x<<","<<st_y<<","<<st_z<<","<<end_x<<","<<end_y<<","<<end_z<<","<<pbdt<<","<<nd<<"\n";
+	   if (isTestSample) fout_test<<tag<<","<<TARGET<<","<<ntrklen<<","<<trklen<<","<<PID<<","<<B<<","<<costheta<<","<<mediandedx<<","<<endpointdedx<<","<<calo<<","<<avcalo<<","<<st_x<<","<<st_y<<","<<st_z<<","<<end_x<<","<<end_y<<","<<end_z<<","<<pbdt<<","<<nd<<"\n";
 	}
   	fout.close();
 	fout_train.close();
